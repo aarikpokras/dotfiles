c4fa5fc3cc25ad9eeb3e6bb7a228e1005cd365eb
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2025-12-22 01:15:11 -0500

    Mark logbrt.sh as obsolete
    Mark logbrt.sh as obsolete due to brightnessctl usage.

--- hypr/logbrt.sh
+++ hypr/logbrt.sh
@@ -1,3 +1,4 @@
+# obsolete with `brightnessctl -e`
 cbrtpct=$(~/.config/waybar/div $(brightnessctl g) $(brightnessctl m))
 nbrtpct=$(( $cbrtpct + 5 ))
 brtpct1=$(~/.config/waybar/div $nbrtpct 100) # decimal (like 0.35)
