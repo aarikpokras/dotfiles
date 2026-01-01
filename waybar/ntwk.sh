e72dc81953e04f28504ca8927d2008d54a79cc62
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2025-12-31 19:29:24 -0500

    Add files via upload

--- waybar/ntwk.sh
+++ waybar/ntwk.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-printf "$(nmcli device wifi list | grep "^\*" | awk '{print $9}')" | sed 's/\*/•/g' | head -n1
+#printf "$(nmcli device wifi list | grep "^\*" | awk '{print $9}')" | sed 's/\*/•/g' | head -n1)"
+printf "$(nmcli -g IN-USE,BARS d wifi list | grep "^\*" | sed -e 's/.*://' -e 's/\*/•/g')"
